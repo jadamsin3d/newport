49e75864490861f4a2b03f9d55a265316b8b2505
Atoms <jadamsin3d@gmail.com>
2019-02-23 21:32:45 -0800

    completely changed

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,27 +1,9 @@
-console.log("hello");
+function openNav() {
+    document.getElementById("mySideNav").style.width = "250px";
+    document.getElementById("maincontent").style.marginLeft = "250px";
+}
 
-var ishovered = false;
-
-$(document).ready(function () {
-    $(".homeBtn").hover(function () {
-        console.log('hello');
-        $(".dropdownHub").slideDown();
-    });
-},
-    function () {
-        var current = $(this);
-        setTimeout(function () {
-            if (!ishovered) {
-                current.siblings(".dropdownHub").slideUp(500);
-            }
-        }, 50);
-    })
-
-$('body').on("mouseleave", ".dropdownHub", function(){
-    ishovered = false;
-    $('.dropdownHub').stop().slideUp(500)
-});
-
-$('body').on("mouseenter", ".dropdownHub", function(){
-    ishovered = true;
-});
+function closeNav() {
+    document.getElementById("mySideNav").style.width = "0";
+    document.getElementById("maincontent").style.marginLeft = "0";
+}
